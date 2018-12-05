cd3ebe2db0a558f2dcd6fa097631decb749a9522
Erkan Ertürk <erkanerturk096@gmail.com>
2018-12-05 22:35:26 +0300

    Added side drawer

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import burgerLogo from '../../assets/images/burger-logo.png';
 
 import classes from './Logo.css';
 
 const logo = (props) => (
-  <div className={classes.Logo}>
+  <div className={classes.Logo} style={{ height: props.height }}>
     <img src={burgerLogo} alt="MyBurger" />
   </div>
 );
+
+logo.propTypes = {
+  height: PropTypes.string,
+};
 
 export default logo;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,8 +8,10 @@ import classes from './Toolbar.css';
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <Logo />
-    <nav>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
       <NavigationItems />
     </nav>
   </header>
